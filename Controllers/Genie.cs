d4921f26b7ee24cace165547a9be774a3d217f88
danielchoike2 <choike3@mail.nmc.edu>
2023-11-20 19:35:11 -0500

    Chapter 10 Additions

--- Controllers/Genie.cs
+++ Controllers/Genie.cs
@@ -30,7 +30,31 @@ namespace InTheBag.Controllers
                 return View("Novice");
             }
         }
+        public IActionResult Create2()
+        {
+            var name = RouteData.Values["GenieName"];
+            var Years = Int32.Parse((string)RouteData.Values["Age"]);
+            var numGranted = Int32.Parse((string)RouteData.Values["WishesGranted"]);
+
+            if (numGranted > 5000 || Years > 1000)
+                return View("ExperiencedGenie");
+            else
+                return View("Novice");
+            }
     
-    
+        public IActionResult Perks()
+        {
+            ViewBag.Posted = false;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Perks(string[] perk)
+        {
+            ViewBag.Posted = true;
+            //ViewBag.Perks = Request.Form["perk"];
+            ViewBag.Perks = perk;
+            return View();
+        }
+
     }
 }
